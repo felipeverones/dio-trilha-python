312844d06dd1c2e89557bf4229e9337c018d3923
felipeverones <felipe_verones@hotmail.com>
2024-05-04 19:22:01 -0300

    desafio 2 python

---
+++ desafio-sistema-bancario-otimizado/README.md
@@ -0,0 +1,49 @@
+
+## Otimizando o Sitema Bancário com Funções Python - DIO Trilha Python
+
+Sistema Bancário simples desenvolvido como Desafio da Trilha Python da DIO: `Otimizando o Sistema Bancário com Funções Python`. 
+
+Permite a criação de usuários e contas, além de realizar operações básicas como depósitos, saques e visualização de extratos.
+
+## Funcionalidades
+
+- **Criação de Usuários**: Adiciona um novo usuário ao sistema.
+- **Criação de Contas**: Associa uma conta bancária a um usuário existente.
+- **Depósito**: Permite depositar dinheiro em uma conta.
+- **Saque**: Permite sacar dinheiro de uma conta, observando limites de saldo e quantidade de saques.
+- **Extrato**: Mostra todas as transações realizadas em uma conta, bem como o saldo atual.
+- **Listagem de Contas**: Lista todas as contas bancárias existentes no sistema.
+
+## Uso
+
+Para executar o programa, basta rodar o script principal. Isso iniciará um loop que apresentará um menu de opções:
+
+```bash
+=============== MENU ================
+[U] Novo Usuário
+[C] Nova Conta
+[D] Depositar
+[S] Sacar
+[E] Extrato
+[L] Listar Contas
+[Q] Sair
+=>
+```
+
+
+Você pode interagir com o sistema escolhendo uma opção e seguindo as instruções na tela.
+
+## Instalação
+
+Não são necessárias bibliotecas externas para executar este programa, apenas uma versão do Python 3.
+
+## Exemplo de Uso
+
+1. Escolha a opção 'U' para criar um novo usuário.
+2. Siga as instruções para fornecer os dados do usuário.
+3. Escolha 'C' para criar uma conta para o usuário.
+4. Use 'D' para depositar dinheiro na conta.
+5. Use 'S' para realizar um saque.
+6. Selecione 'E' para ver o extrato da conta.
+7. Use 'L' para listar todas as contas.
+
--- desafio-sistema-bancario-otimizado/sistema-bancario.py
+++ desafio-sistema-bancario-otimizado/sistema-bancario.py
@@ -4,13 +4,13 @@ import textwrap
 def menu():
     menu = """\n
     ================ MENU ================
-    [d]\tDepositar
-    [s]\tSacar
-    [e]\tExtrato
-    [nc]\tNova conta
-    [lc]\tListar contas
-    [nu]\tNovo usuário
-    [q]\tSair
+    [U]\tNovo Usuário
+    [C]\tNova Conta
+    [D]\tDepositar
+    [S]\tSacar
+    [E]\tExtrato
+    [L]\tListar Contas
+    [Q]\tSair
     => """
     return input(textwrap.dedent(menu))
 
@@ -21,7 +21,7 @@ def depositar(saldo, valor, extrato, /):
         extrato += f"Depósito:\tR$ {valor:.2f}\n"
         print("\n=== Depósito realizado com sucesso! ===")
     else:
-        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+        print("\n Operação falhou! O valor informado é inválido. ")
 
     return saldo, extrato
 
@@ -32,13 +32,13 @@ def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
     excedeu_saques = numero_saques >= limite_saques
 
     if excedeu_saldo:
-        print("\n@@@ Operação falhou! Você não tem saldo suficiente. @@@")
+        print("\n Operação falhou! Você não tem saldo suficiente. ")
 
     elif excedeu_limite:
-        print("\n@@@ Operação falhou! O valor do saque excede o limite. @@@")
+        print("\n Operação falhou! O valor do saque excede o limite. ")
 
     elif excedeu_saques:
-        print("\n@@@ Operação falhou! Número máximo de saques excedido. @@@")
+        print("\n Operação falhou! Número máximo de saques excedido. ")
 
     elif valor > 0:
         saldo -= valor
@@ -47,7 +47,7 @@ def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
         print("\n=== Saque realizado com sucesso! ===")
 
     else:
-        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+        print("\n Operação falhou! O valor informado é inválido. ")
 
     return saldo, extrato
 
@@ -64,7 +64,7 @@ def criar_usuario(usuarios):
     usuario = filtrar_usuario(cpf, usuarios)
 
     if usuario:
-        print("\n@@@ Já existe usuário com esse CPF! @@@")
+        print("\n Já existe usuário com esse CPF! ")
         return
 
     nome = input("Informe o nome completo: ")
@@ -89,7 +89,7 @@ def criar_conta(agencia, numero_conta, usuarios):
         print("\n=== Conta criada com sucesso! ===")
         return {"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario}
 
-    print("\n@@@ Usuário não encontrado, fluxo de criação de conta encerrado! @@@")
+    print("\n Usuário não encontrado, fluxo de criação de conta encerrado! ")
 
 
 def listar_contas(contas):
@@ -117,12 +117,12 @@ def main():
     while True:
         opcao = menu()
 
-        if opcao == "d":
+        if opcao == "d" or opcao == "D":
             valor = float(input("Informe o valor do depósito: "))
 
             saldo, extrato = depositar(saldo, valor, extrato)
 
-        elif opcao == "s":
+        elif opcao == "s" or opcao == "S":
             valor = float(input("Informe o valor do saque: "))
 
             saldo, extrato = sacar(
@@ -134,23 +134,23 @@ def main():
                 limite_saques=LIMITE_SAQUES,
             )
 
-        elif opcao == "e":
+        elif opcao == "e" or opcao == "E":
             exibir_extrato(saldo, extrato=extrato)
 
-        elif opcao == "nu":
+        elif opcao == "u" or opcao == "U":
             criar_usuario(usuarios)
 
-        elif opcao == "nc":
+        elif opcao == "c" or opcao == "C":
             numero_conta = len(contas) + 1
             conta = criar_conta(AGENCIA, numero_conta, usuarios)
 
             if conta:
                 contas.append(conta)
 
-        elif opcao == "lc":
+        elif opcao == "l" or opcao == "L":
             listar_contas(contas)
 
-        elif opcao == "q":
+        elif opcao == "q" or opcao == "Q":
             break
 
         else:
